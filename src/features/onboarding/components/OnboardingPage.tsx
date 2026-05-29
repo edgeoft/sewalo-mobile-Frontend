442c0a52171f81f3beddd5d89e908b84f020d835
Ashitom Budhathoki <ashitom@edgeoft.com>
2026-05-29 22:45:41 +0545

    chore: update onboarding text size

--- src/features/onboarding/components/OnboardingPage.tsx
+++ src/features/onboarding/components/OnboardingPage.tsx
@@ -15,13 +15,13 @@ export default function OnboardingPage({
 
   return (
     <View style={{ width, height: '100%' }} className="items-center justify-start py-6 px-6 bg-transparent">
-      <View className="w-full h-72 justify-center items-center bg-transparent border-0">
+      <View className="flex-1 w-full justify-center items-center bg-transparent border-0 mb-6">
         <Image source={illustration} className="w-full h-full border-0" resizeMode="contain" />
       </View>
 
-      <View className="items-center justify-start w-full px-2 mt-auto h-28">
-        <Text className="text-gray-900 text-[26px] font-sans-bold text-center mb-3 leading-tight pt-1">{title}</Text>
-        <Text className="text-gray-600 text-[14px] font-sans-medium text-center leading-5.5">{description}</Text>
+      <View className="items-center justify-start w-full px-2 h-28">
+        <Text className="text-gray-900 text-[22px] font-sans-bold text-center mb-3 leading-tight pt-1">{title}</Text>
+        <Text className="text-gray-600 text-[13px] font-sans-medium text-center leading-5.5">{description}</Text>
       </View>
     </View>
   );
